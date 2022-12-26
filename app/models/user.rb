5f891c80aa8398ae3d55da57bf63c97af5d01fbb
Felix A Torrenegra G <f.torrenegrag@gmail.com>
2022-12-26 12:54:43 -0500

    set revocation strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  before_create :jti_token
+
+  def jwt_payload
+    super.merge({access_level: access_level})
+  end
+
+  private
+
+  def jti_token
+    SecureRandom.uuid
+  end
 end
