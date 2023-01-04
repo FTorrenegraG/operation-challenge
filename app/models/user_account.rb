e903eafd2dacbdc645ab8bc605f3b58680d5c06f
Felix A Torrenegra G <f.torrenegrag@gmail.com>
2023-01-04 15:16:12 -0500

    add out_date_after_in_date validation in user_account model

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -4,6 +4,7 @@ class UserAccount < ApplicationRecord
   belongs_to :user
   belongs_to :account
 
+  validate :out_date_after_in_date
   validate :last_movement_duplicated?, on: :create
 
   before_create :end_last_movement
@@ -21,12 +22,18 @@ class UserAccount < ApplicationRecord
 
   private
 
+  def out_date_after_in_date
+    return if out_date.blank? || in_date.blank?
+    return true if in_date < out_date
+
+    errors.add(:out_date, 'must be after the in date')
+  end
+
   def last_movement_duplicated?
     last_movement = UserAccount.where(user_id:, account_id:).last
     return true unless last_movement
 
     errors.add(:account_id, 'Your last movement active is in the same account')
-    false
   end
 
   def end_last_movement
