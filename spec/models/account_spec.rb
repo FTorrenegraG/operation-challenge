047e6072e39ebd125f925cc0ed2a6717a6e68128
Felix A Torrenegra G <f.torrenegrag@gmail.com>
2023-01-06 19:19:59 -0500

    account rspec test model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe(Account, type: :model) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'name validation' do
+      let!(:account) { create(:account) }
+      let!(:new_account) { build(:account, name: account.name) }
+      it 'should be invalid' do
+        expect(new_account).to_not(be_valid)
+      end
+
+      it 'should include error message in name' do
+        new_account.valid?
+        expect(new_account.errors[:name].first).to(eq('has already been taken'))
+      end
+    end
+  end
 end
